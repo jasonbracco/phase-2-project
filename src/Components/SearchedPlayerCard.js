d8e410aef5a86b41d9b56f34a6a86cad7ea54b4b
Jason Bracco <bracco.jason@gmail.com>
2022-08-07 17:06:55 -0400

    conditionally render pitcherback or hitterback on click

--- src/Components/SearchedPlayerCard.js
+++ src/Components/SearchedPlayerCard.js
@@ -7,6 +7,9 @@ function SearchedPlayerCard({player}){
 
     const [hitterStats, setHitterStats]=useState([])
     const [pitcherStats, setPitcherStats]=useState([])
+    const [flipped, setFlipped]=useState(true)
+    const [isPitcher, setIsPitcher]=useState(true)
+
 
     const {id, bats, throws, image, firstName, lastName, number, position, fullName} = player
 
@@ -15,37 +18,50 @@ function SearchedPlayerCard({player}){
             fetch(`http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season='2021'&player_id=${id}`)
             .then(response => response.json())
             .then((stats) => setHitterStats(stats.sport_hitting_tm.queryResults.row))
+            setIsPitcher(!isPitcher)
         }
         else {
             fetch(`http://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id='mlb'&game_type='R'&season='2021'&player_id=${id}`)
             .then(response => response.json())
-            .then((stats) => setPitcherStats(stats.sport_hitting_tm.queryResults.row))
+            .then((stats) => setPitcherStats(stats.sport_pitching_tm.queryResults.row))
             }
-    },[position, id])
+    },[position, id, isPitcher])
 
     function handleFlipped(){
-        if (position==="P"){
-            return <PitcherBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
-            pitcherStats={pitcherStats}/>
-        }
-        else {
-            return <HitterBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
-            hitterStats={hitterStats}/>
-        }
+        setFlipped(!flipped)
     }
+
+
+    // function backSideFunction(){
+    //     if (position==="P"){
+    //         return <PitcherBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
+    //         pitcherStats={pitcherStats}/>
+    //     }
+    //     else {
+    //         <HitterBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
+    //             hitterStats={hitterStats}/>
+    //     }
+    // }
 
     return(
         <div className="player_card" onClick={handleFlipped}>
-            <div className="front">
-                <img src={image} alt={fullName} className="player_image"/>
-                <div className="card_content">
-                    <h4>{firstName} {lastName}</h4>
-                    <p>Number {number}</p>
-                    <p>Position: {position}</p>
-                    <p>Bats: {bats}</p>
-                    <p>Throws: {throws}</p>
-                </div>
-            </div>
+            {flipped ? 
+                <div className="front">
+                    <img src={image} alt={fullName} className="player_image"/>
+                    <div className="card_content">
+                        <h4>{firstName} {lastName}</h4>
+                        <p>Number {number}</p>
+                        <p>Position: {position}</p>
+                        <p>Bats: {bats}</p>
+                        <p>Throws: {throws}</p>
+                    </div>
+                </div>:
+                isPitcher ?
+                <PitcherBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
+                pitcherStats={pitcherStats}/>:
+                <HitterBack image={image} fullName={fullName} firstName={firstName} lastName={lastName}
+                hitterStats={hitterStats}/>
+            }
         </div>
 
     )
